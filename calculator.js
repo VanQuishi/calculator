0c02bef7f7d80387d11e7c27c4ace093a14a50bc
Chau Nguyen <songchau99@gmail.com>
2020-04-12 21:12:06 -0500

    Added code to evaluate expression. Dont know how to evaluate after that yet.

--- calculator.js
+++ calculator.js
@@ -34,11 +34,9 @@ const buttons = document.querySelectorAll('.btn');
 const clear = document.querySelector('.clear');
 const clearAll = document.querySelector('.clearAll');
 const equalSign = document.querySelector('.equalSign');
-var a = 0;
-var b = 0;
-var operator;
 var expression;
 var currentDisplay;
+var solution;
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -63,7 +61,9 @@ clearAll.addEventListener('click', (e) =>
 equalSign.addEventListener('click',(e) => {
     display.textContent += equalSign.textContent;
     expression = display.textContent;
-    //processExpression(expression);
+    console.log(expression);
+    solution = processExpression(expression);
+    console.log(solution);
 });
 
 function isOperator (character) {
@@ -75,7 +75,26 @@ function isOperator (character) {
     return false;
 }
 
+function peak(arr) {
+    return arr[arr.length - 1];
+}
+
+function hasHigherPrecedence (currentOp, stackOp) {
+    if (currentOp == '*' || currentOp == '/' && stackOp == '+' || stackOp == '-') {
+        return true;
+    }
+    
+    return false;
+}
+
 function processExpression(expression) {
+
+    //check if the expression has a number before equal sign or not
+    //this check prevents invalid expression from user
+    if (isOperator(expression[expression.length - 2])) {
+        console.log("invalid expression");
+        return;
+    }
 
     let i = 0;
     let numArr = [];
@@ -92,26 +111,89 @@ function processExpression(expression) {
         else {
             if (temp != '') {
                 numArr.push(temp);
+                console.log("inserting in numArr: " + temp);
+                temp = '';
+               
             }
 
-            if (opArr == []) {
+            if (opArr.length == 0) {
                 opArr.push(expression[i]);
+                console.log("inserting in opArr: " + expression[i]);
+            }
+
+            else if (opArr.length != 0) {
+                if (!hasHigherPrecedence(expression[i], peak(opArr))) {
+                    let num2 = Number(numArr.pop());
+                    console.log("removing in numArr: " + num2);
+                    let num1 = Number(numArr.pop());
+                    console.log("removing in numArr: " + num1);
+
+                    let op = opArr.pop();
+                    console.log("removing in opArr: " + op);
+
+                    let result = operate(num1,op,num2);
+                    //console.log("perform: " + num1 + op + num2 + " equals " + result);
+
+                    numArr.push(result);
+                    console.log("inserting in numArr: " + result);
+
+                    opArr.push(expression[i]);
+                    console.log("inserting in opArr: " + expression[i]);
+                }
+
+                else {
+                    opArr.push(expression[i]);
+                    console.log("inserting in opArr: " + expression[i]);
+                }
             }
         }
         
         i++;
     }
 
-    //===for debug purpose===
+    if (!isOperator(temp) && temp != '') {
+        numArr.push(temp);
+        console.log("inserting in numArr: " + temp);
+        temp='';
+    }
+
+     //===for debug purpose===
     
     /*console.log("In num stack: ");
-    for (let j = 0; j < numArr.length; i++) {
+    for (let j = 0; j < numArr.length; j++) {
         console.log(numArr[j]);
     }
 
     console.log("in operator stack: ");
 
-    for (let j = 0; j < numArr.length; i++) {
-        console.log(numArr[j]);
+    for (let j = 0; j < opArr.length; j++) {
+        console.log(opArr[j]);
     }*/
+
+    /*while (numArr.length > 1) {
+        let num2 = Number(numArr.pop());
+        console.log("removing in numArr: " + num2);
+        let num1 = Number(numArr.pop());
+        console.log("removing in numArr: " + num1);
+
+        let op = opArr.pop();
+        console.log("removing in opArr: " + op);
+
+        let result = operate(num1,op,num2);
+
+        numArr.push(result);
+        console.log("inserting in numArr: " + result);
+    }*/
+
+    return result;
+
+   
 }
+
+/*let test = [0,1,2];
+console.log(peak(test));*/
+
+/*console.log(hasHigherPrecedence('*', '-'));
+console.log(hasHigherPrecedence('/', '*'));
+console.log(hasHigherPrecedence('-', '*'));
+console.log(hasHigherPrecedence('-', '+'));*/
